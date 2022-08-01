dcfb306e93507ef592ef6fb19130e34b723a075d
adriana mihalescu <adriana.mihalescu@gmail.com>
2022-08-01 17:45:18 +0100

    add basic test structure

---
+++ cypress/e2e/firstTest.spec.js
@@ -0,0 +1,9 @@
+const { iterator } = require("rxjs/internal-compatibility");
+
+describe("Our first suite", () => {
+  it("first test", () => {});
+
+  it("second test", () => {});
+
+  it("third test", () => {});
+});
