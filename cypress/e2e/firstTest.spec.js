38dd895e2959ad7cf722bbd69992cfca86c5f905
Adriana M <adriana@calmlyresolve.com>
2022-08-03 16:18:57 +0100

    add types of locators

--- cypress/e2e/firstTest.spec.js
+++ cypress/e2e/firstTest.spec.js
@@ -1,7 +1,24 @@
-const { iterator } = require("rxjs/internal-compatibility");
-
+/// <reference types="cypress" />
 describe("Our first suite", () => {
-  it("first test", () => {});
+  it("first test", () => {
+    //by Tag name
+    cy.get("input");
+
+    //by ID
+    cy.get("#inputEmail");
+
+    //by class name
+    cy.get(".input-full-width");
+
+    //by Atribute Name
+    cy.get("[placeholder]");
+
+    //by Attribute name and value
+    cy.get("[placeholder="Email"]");
+
+    //by class value
+    cy.get('[class="input-full-width size-medium shape-rectangle"]')
+  });
 
   it("second test", () => {});
 
