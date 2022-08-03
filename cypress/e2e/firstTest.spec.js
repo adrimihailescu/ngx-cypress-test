d067c7ac580d3dafac7b0ed5b19e917b4c887653
Adriana M <adriana@calmlyresolve.com>
2022-08-03 17:19:32 +0100

    add first test run and passed

--- cypress/e2e/firstTest.spec.js
+++ cypress/e2e/firstTest.spec.js
@@ -1,11 +1,14 @@
 /// <reference types="cypress" />
 describe("Our first suite", () => {
   it("first test", () => {
+    cy.visit("/");
+    cy.contains("Forms").click();
+    cy.contains("Form Layouts").click();
     //by Tag name
     cy.get("input");
 
     //by ID
-    cy.get("#inputEmail");
+    cy.get("#inputEmail1");
 
     //by class name
     cy.get(".input-full-width");
@@ -14,10 +17,22 @@ describe("Our first suite", () => {
     cy.get("[placeholder]");
 
     //by Attribute name and value
-    cy.get("[placeholder="Email"]");
+    cy.get('[placeholder="Email"]');
 
     //by class value
-    cy.get('[class="input-full-width size-medium shape-rectangle"]')
+    cy.get('[class="input-full-width size-medium shape-rectangle"]');
+
+    //by tag name and attribute with value
+    cy.get('input[placeholder="Email"]');
+
+    //by two different attributes
+    cy.get('[placeholder="Email"][type="email"]');
+
+    // by tag name, Attribute with value, ID and class name
+    cy.get('input[placeholder="Email"]#inputEmail.input-full-width');
+
+    //The most recommanded way by Cypress
+    cy.get('[data-cy="imputEmail1"]');
   });
 
   it("second test", () => {});
