aa6a0d76764b73daf086e4e509effff4d7a5fbb0
Adriana M <adriana@calmlyresolve.com>
2022-08-05 18:05:08 +0100

    add examples for working with lists and dropdowns

--- cypress/e2e/firstTest.spec.js
+++ cypress/e2e/firstTest.spec.js
@@ -161,7 +161,7 @@ describe("Our first suite", () => {
   });
 
   //checkboxes
-  it.only("check boxes", () => {
+  it("check boxes", () => {
     cy.visit("/");
     cy.contains("Modal & Overlays").click();
     cy.contains("Toastr").click();
@@ -169,5 +169,45 @@ describe("Our first suite", () => {
     // cy.get('[type="checkbox"]').check({ force: true });
     // cy.get('[type="checkbox"]').eq(0).check({ force: true });
     cy.get('[type="checkbox"]').eq(0).check({ force: true });
+  });
+
+  //lists and dropdown menu
+  it.only("lists and dropdown", () => {
+    cy.visit("/");
+
+    //1
+    // cy.get("nav nb-select").click();
+    // cy.get(".options-list").contains("Dark").click();
+    // cy.get("nb-layout-header nav").should(
+    //   "have.css",
+    //   "background-color",
+    //   "rgb(34, 43, 69)"
+    // );
+
+    //2nd example
+    cy.get("nav nb-select").then((dropdown) => {
+      cy.wrap(dropdown).click();
+      cy.get(".options-list nb-option").each((listItem, index) => {
+        const itemText = listItem.text().trim();
+
+        const colors = {
+          Light: "rgb(255, 255, 255)",
+          Dark: "rgb(34, 43, 69)",
+          Cosmic: "rgb(50, 50, 89)",
+          Corporate: "rgb(255, 255, 255)",
+        };
+
+        cy.wrap(listItem).click();
+        cy.wrap(dropdown).should("contain", itemText);
+        cy.get("nb-layout-header nav").should(
+          "have.css",
+          "background-color",
+          colors[itemText]
+        );
+        if (index < 3) {
+          cy.wrap(dropdown).click();
+        }
+      });
+    });
   });
 });
